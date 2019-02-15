3fea3099b6d3e8a63c3a38c4fb8a1233f48ed1c7
Achref Meghirbi <medachref.meghirbi@esprit.tn>
2019-02-16 00:04:22 +0100

    inital commit

---
+++ FoodTrucker/FoodTrucker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrucker
+//
+//  Created by Achref Meghirbi on 2/15/19.
+//  Copyright © 2019 Achref Meghirbi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
