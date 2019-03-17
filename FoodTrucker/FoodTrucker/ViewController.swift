23ba4d6a48a87210109ed5c6128bea33b4883d77
Achref Meghirbi <medachref.meghirbi@esprit.tn>
2019-03-17 23:07:42 +0100

    connect storyboard to source code

--- FoodTrucker/FoodTrucker/ViewController.swift
+++ FoodTrucker/FoodTrucker/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    // MARK : proprities
+    
+    
+    @IBOutlet weak var mealName: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var mealLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mealName.delegate = self
     }
 
+    // MARK : actions
+    @IBAction func SaveAction(_ sender: UIButton) {
+        mealLabel.text = mealName.text
+    }
+    
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
 
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealLabel.text = mealName.text
+    }
 }
 
